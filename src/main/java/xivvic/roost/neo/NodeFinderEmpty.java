e4e07b8e3b3b1d8045ea123b2c3b80e44df3d697
Reid <reid@foobox.com>
2016-02-18 14:34:27 -0600

    doc

--- src/main/java/xivvic/roost/neo/NodeFinderEmpty.java
+++ src/main/java/xivvic/roost/neo/NodeFinderEmpty.java
@@ -3,8 +3,18 @@ package xivvic.roost.neo;
 import com.google.auto.value.AutoValue;
 
 /**
- * NodeFinder that serves as when the value for the given property is
+ * NodeFinder that serves when the value for the given property is
  * not known.  This is an immutable class which is always invalid.
+ * 
+ * A NodeFinder needs 3 pieces of information:
+ * 
+ * Label
+ * PropertyName
+ * PropertyValue
+ * 
+ * When the first two are available but the 3rd is unknown, this class
+ * will aggregate those two values for when the final information is available.
+ * 
  * 
  * @author Reid
  */
